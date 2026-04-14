073d7feb1de38d56546c816aed21d7577d76f6dc
Jingyu Zhong <93188158+zhongjingyu1@users.noreply.github.com>
2026-04-14 21:03:28 +0800

    Add files via upload

--- main/datasets.py
+++ main/datasets.py
@@ -3,28 +3,34 @@ from torch.utils.data import Dataset
 from imagelist import *
 
 class CustomDataset(Dataset):
-    """Dataset.
-    """
+    """Dataset."""
 
     def __init__(self, dataset, split):
         assert dataset in ["coco-lt", "voc-lt", "voc", "nus-wide"]
         if dataset == 'coco-lt':
             self.data_source = ImageList(root='../data/coco/',
-                                        list_file='../appendix/coco/coco_lt_%s.txt' % split,
-                                        label_file='../appendix/coco/coco_labels.txt',
+                                        list_file='../data/coco/coco_lt_%s.txt' % split,
+                                        label_file='../data/coco/coco_labels.txt',
                                         nb_classes=80,
                                         split=split)
         elif dataset == 'voc-lt':
             self.data_source = ImageList(root='../data/voc/',
-                                        list_file='../appendix/VOCdevkit/voc_lt_%s.txt' % split,
-                                        label_file='../appendix/VOCdevkit/voc_labels.txt',
+                                        list_file='../data/voc/voc_lt_%s.txt' % split,
+                                        label_file='../data/voc/voc_labels.txt',
                                         nb_classes=20,
                                         split=split)
+        elif dataset == 'nus-wide':
+            self.data_source = ImageList(root='../data/nuswide/',
+                                        list_file='../data/nuswide/nw_lt_%s.txt' % split,
+                                        label_file='../data/nuswide/nw_labels.txt',
+                                        nb_classes=81,
+                                        split=split)
 
-        self.targets = self.data_source.labels # one-hot label
+        self.targets = self.data_source.labels  # one-hot label
         self.categories = self.data_source.categories
         self.fns = self.data_source.fns
         self.ind1 = self.data_source.ind1
+
     def __len__(self):
         return self.data_source.get_length()
 
@@ -34,28 +40,34 @@ class CustomDataset(Dataset):
 
 
 class CustomDataset_partial(Dataset):
-    """Dataset.
-    """
+    """Dataset."""
 
     def __init__(self, dataset, split, given_label_matrix):
         assert dataset in ["coco-lt", "voc-lt", "voc", "nus-wide"]
         if dataset == 'coco-lt':
             self.data_source = ImageList(root='../data/coco/',
-                                         list_file='../appendix/coco/coco_lt_%s.txt' % split,
-                                         label_file='../appendix/coco/coco_labels.txt',
+                                         list_file='../data/coco/coco_lt_%s.txt' % split,
+                                         label_file='../data/coco/coco_labels.txt',
                                          nb_classes=80,
                                          split=split)
         elif dataset == 'voc-lt':
             self.data_source = ImageList(root='../data/voc/',
-                                         list_file='../appendix/VOCdevkit/voc_lt_%s.txt' % split,
-                                         label_file='../appendix/VOCdevkit/voc_labels.txt',
+                                         list_file='../data/voc/voc_lt_%s.txt' % split,
+                                         label_file='../data/voc/voc_labels.txt',
                                          nb_classes=20,
+                                         split=split)
+        elif dataset == 'nus-wide':
+            self.data_source = ImageList(root='../data/nuswide/',
+                                         list_file='../data/nuswide/nw_lt_%s.txt' % split,
+                                         label_file='../data/nuswide/nw_labels.txt',
+                                         nb_classes=81,
                                          split=split)
 
         self.targets = self.data_source.labels  # one-hot label
         self.categories = self.data_source.categories
         self.fns = self.data_source.fns
         self.given_label_matrix = given_label_matrix
+        self.ind1 = self.data_source.ind1
 
     def __len__(self):
         return self.data_source.get_length()
@@ -65,38 +77,28 @@ class CustomDataset_partial(Dataset):
         partial_label = self.given_label_matrix[idx]
         return img, target, partial_label, idx
 
+
 def build_dataset1(dataset, split):
     assert split in ['train', 'test', 'val']
+    assert dataset in ["coco-lt", "voc-lt", "nus-wide"]
 
-    assert dataset in ["coco-lt", "voc-lt"]
-    if split == 'train':
-        dataset = CustomDataset(
-        dataset=dataset,
-        split=split
-        )
-    elif split == 'test':
-        dataset = CustomDataset(
-        dataset=dataset,
-        split=split
-        )
+    if split == 'val':
+        split = 'test'
 
+    dataset = CustomDataset(dataset=dataset, split=split)
     return dataset
+
 
 def build_dataset_partial(dataset, split, given_label_matrix=None):
     assert split in ['train', 'test', 'val']
+    assert dataset in ["coco-lt", "voc-lt", "nus-wide"]
 
-    assert dataset in ["coco-lt", "voc-lt"]
-    if split == 'train':
-        dataset = CustomDataset_partial(
+    if split == 'val':
+        split = 'test'
+
+    dataset = CustomDataset_partial(
         dataset=dataset,
         split=split,
         given_label_matrix=given_label_matrix
-        )
-    elif split == 'test':
-        dataset = CustomDataset_partial(
-        dataset=dataset,
-        split=split,
-        given_label_matrix=given_label_matrix
-        )
-
+    )
     return dataset
